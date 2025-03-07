6738dc9cca37dd38df6f6c7f6d3e7f120f6c0f1f
MFaisal125 <mfaisal0125470@gmail.com>
2025-03-07 13:10:12 +0500

    E-commerce

--- client/src/components/home/NewArrivals.js
+++ client/src/components/home/NewArrivals.js
@@ -26,9 +26,7 @@ const NewArrivals = () => {
   return (
     <Container>
       <section className="flex flex-col gap-y-10">
-        <h1 className="text-4xl">
-          New Arrivals. <span className="">New Equipment</span>
-        </h1>
+        <h1 className="text-4xl">New Arrivals</h1>
 
         <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-x-6 gap-y-8">
           {productsLoading ? (
